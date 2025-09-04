d9f1f72015b48156b944761b3a2612e1900fe21e
Subhan <subhanakbar346@gmail.com>
2025-09-04 20:39:28 +0500

    did some math exersice

--- src/MathExersice.java
+++ src/MathExersice.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class MathExersice {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        double a;
+        double b;
+
+
+        System.out.print("What is a:");
+        a = scanner.nextDouble();
+
+        System.out.print("What is b");
+        b = scanner.nextDouble();
+
+        double c = java.lang.Math.sqrt(java.lang.Math.pow(a, 2) + java.lang.Math.pow(b, 2) );
+        System.out.println("The hypotenuse c is: " +c);
+
+        scanner.close();
+    }
 }
