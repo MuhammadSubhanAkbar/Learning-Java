4d52d0015150832a60eae727318078f3e44b45ae
Subhan Akbar <subhanakbar346@gmail.com>
2025-10-02 16:21:06 +0500

    learned abstraction

---
+++ src/Circle.java
@@ -0,0 +1,14 @@
+import java.lang.Math;
+
+public class Circle extends Shape {
+    double radius;
+
+    Circle(double radius){
+        this.radius = radius;
+    }
+
+    @Override
+    double area() {
+        return Math.PI * Math.pow(radius,2);
+    }
+}
---
+++ src/Shape.java
@@ -0,0 +1,8 @@
+public abstract class Shape {
+
+    abstract double area();//This is an abstract method
+
+    void display(){
+        System.out.println("This is a shape");
+    }
+}
